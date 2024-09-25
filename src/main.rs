b9241f1056503f9261dc439c1dc1cb0f2f289ced
yikejason <18215513386@163.com>
2024-09-25 14:07:02 +0800

    feature: add a basic flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Search query string {}", query);
+    println!("In file path {}", file_path);
+
+    let contents = fs::read_to_string(file_path).unwrap();
+
+    println!("with text:\n{contents}");
 }
