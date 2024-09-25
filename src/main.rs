ff21493e82462e4f6b159147070471e46dfcd5ef
yikejason <18215513386@163.com>
2024-09-25 14:51:34 +0800

    feature: add err handle and add project's main flow into the lib.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,20 @@
-use std::{env, fs};
+use std::{env, process};
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let file_path: String = args[2].clone();
-
-        Config { query, file_path }
-    }
-}
-
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.file_path).unwrap();
-
-    println!("with text:\n{contents}");
-}
+use minigrep::{run, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parse arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Search query string {}", config.query);
     println!("In file path {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
