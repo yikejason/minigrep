504ff9fc4e2796fda46aec611f56ba1b80bd49f3
yikejason <18215513386@163.com>
2024-09-25 14:30:26 +0800

    feature: Add a function to extract and parse configuration parameters, and a function to execute them.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 use std::{env, fs};
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path: String = args[2].clone();
+
+        Config { query, file_path }
+    }
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file_path).unwrap();
+
+    println!("with text:\n{contents}");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Search query string {}", query);
-    println!("In file path {}", file_path);
+    println!("Search query string {}", config.query);
+    println!("In file path {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).unwrap();
-
-    println!("with text:\n{contents}");
+    run(config);
 }
